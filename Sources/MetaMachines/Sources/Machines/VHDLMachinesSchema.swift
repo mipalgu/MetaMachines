4bd72b3ce4a2dadc28fa230e805ce0aa7950d7bf
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-05-31 20:12:27 +1000

    stuff.

---
+++ Sources/MetaMachines/Sources/Machines/VHDLMachinesSchema.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Morgan McColl on 31/5/21.
+//
+
+import Attributes
+
+struct VHDLMachinesSchema: SchemaProtocol {
+    
+    typealias Root = Machine
+    
+}
