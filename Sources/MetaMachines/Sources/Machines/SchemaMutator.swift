807277028867e8a85c76c6e4b1386b39ce96a667
Callum McColl <callum.mccoll1@gmail.com>
2021-06-17 12:47:06 +1000

    Don't allow triggers to execute when the path doesn't match

--- Sources/MetaMachines/Sources/Machines/SchemaMutator.swift
+++ Sources/MetaMachines/Sources/Machines/SchemaMutator.swift
@@ -61,28 +61,28 @@ struct SchemaMutator<Schema: MachineSchema>: MachineMutatorResponder, MachineMod
     
     func didAddItem<Path: PathProtocol, T>(_ item: T, to attribute: Path, machine: inout Machine) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == Machine, Path.Value == [T] {
         let trigger = findTrigger(path: attribute)
-        return trigger.performTrigger(&machine)
+        return trigger.performTrigger(&machine, for: AnyPath(attribute))
     }
     
     func didDeleteItems<Path: PathProtocol, T>(table attribute: Path, indices: IndexSet, machine: inout Machine, items: [T]) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == Machine, Path.Value == [T] {
         let indexes = Array<Int>(indices)
         let trigger = AnyTrigger<Machine>(indexes.map { findTrigger(path: attribute[$0]) })
-        return trigger.performTrigger(&machine)
+        return trigger.performTrigger(&machine, for: AnyPath(attribute))
     }
     
     func didDeleteItem<Path: PathProtocol, T>(attribute: Path, atIndex: Int, machine: inout Machine, item: T) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == Machine, Path.Value == [T] {
         let trigger: AnyTrigger<Machine> = findTrigger(path: attribute)
-        return trigger.performTrigger(&machine)
+        return trigger.performTrigger(&machine, for: AnyPath(attribute))
     }
     
     func didMoveItems<Path: PathProtocol, T>(attribute: Path, machine: inout Machine, from source: IndexSet, to destination: Int, items: [T]) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == Machine, Path.Value == [T] {
         let trigger = findTrigger(path: attribute)
-        return trigger.performTrigger(&machine)
+        return trigger.performTrigger(&machine, for: AnyPath(attribute))
     }
     
     func didModify<Path: PathProtocol>(attribute: Path, oldValue: Path.Value, newValue: Path.Value, machine: inout Machine) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == Machine {
         let trigger = findTrigger(path: attribute)
-        return trigger.performTrigger(&machine)
+        return trigger.performTrigger(&machine, for: AnyPath(attribute))
     }
     
     func validate(machine: Machine) throws {
