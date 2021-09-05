b77f9d0183078424974f0512956f2f90b418325f
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-09-05 12:16:19 +1000

    Made return success true to debug redraw bug.

--- Sources/MetaMachines/SchemaMutator.swift
+++ Sources/MetaMachines/SchemaMutator.swift
@@ -18,58 +18,72 @@ struct SchemaMutator<Schema: MachineSchema>: MachineMutatorResponder, MachineMod
     
     func didCreateDependency(machine: inout MetaMachine, dependency: MachineDependency, index: Int) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didCreateDependency(machine: &machine, dependency: dependency, index: index)
+        return .success(true)
     }
     
     func didCreateNewState(machine: inout MetaMachine, state: State, index: Int) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didCreateNewState(machine: &machine, state: state, index: index)
+        return .success(true)
     }
     
     func didCreateNewTransition(machine: inout MetaMachine, transition: Transition, stateIndex: Int, transitionIndex: Int) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didCreateNewTransition(machine: &machine, transition: transition, stateIndex: stateIndex, transitionIndex: transitionIndex)
+        return .success(true)
     }
     
     func didDeleteDependencies(machine: inout MetaMachine, dependency: [MachineDependency], at: IndexSet) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didDeleteDependencies(machine: &machine, dependency: dependency, at: at)
+        return .success(true)
     }
     
     func didDeleteStates(machine: inout MetaMachine, state: [State], at: IndexSet) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didDeleteStates(machine: &machine, state: state, at: at)
+        return .success(true)
     }
     
     func didDeleteTransitions(machine: inout MetaMachine, transition: [Transition], stateIndex: Int, at: IndexSet) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didDeleteTransitions(machine: &machine, transition: transition, stateIndex: stateIndex, at: at)
+        return .success(true)
     }
     
     func didDeleteDependency(machine: inout MetaMachine, dependency: MachineDependency, at: Int) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didDeleteDependency(machine: &machine, dependency: dependency, at: at)
+        return .success(true)
     }
     
     func didDeleteState(machine: inout MetaMachine, state: State, at: Int) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didDeleteState(machine: &machine, state: state, at: at)
+        return .success(true)
     }
     
     func didDeleteTransition(machine: inout MetaMachine, transition: Transition, stateIndex: Int, at: Int) -> Result<Bool, AttributeError<MetaMachine>> {
         schema.didDeleteTransition(machine: &machine, transition: transition, stateIndex: stateIndex, at: at)
+        return .success(true)
     }
     
     func didAddItem<Path: PathProtocol, T>(_ item: T, to attribute: Path, machine: inout MetaMachine) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == MetaMachine, Path.Value == [T] {
-        return schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        return .success(true)
     }
     
     func didDeleteItems<Path: PathProtocol, T>(table attribute: Path, indices: IndexSet, machine: inout MetaMachine, items: [T]) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == MetaMachine, Path.Value == [T] {
-        return schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        return .success(true)
     }
     
     func didDeleteItem<Path: PathProtocol, T>(attribute: Path, atIndex: Int, machine: inout MetaMachine, item: T) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == MetaMachine, Path.Value == [T] {
-        return schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        return .success(true)
     }
     
     func didMoveItems<Path: PathProtocol, T>(attribute: Path, machine: inout MetaMachine, from source: IndexSet, to destination: Int, items: [T]) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == MetaMachine, Path.Value == [T] {
-        return schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        return .success(true)
     }
     
     func didModify<Path: PathProtocol>(attribute: Path, oldValue: Path.Value, newValue: Path.Value, machine: inout MetaMachine) -> Result<Bool, AttributeError<Path.Root>> where Path.Root == MetaMachine {
-        return schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        schema.trigger.performTrigger(&machine, for: AnyPath(attribute))
+        return .success(true)
     }
     
     func validate(machine: MetaMachine) throws {
