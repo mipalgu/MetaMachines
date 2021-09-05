342089d34fef62d4f8b58e9714f4a07980687924
Callum McColl <callum.mccoll1@gmail.com>
2021-09-05 21:28:26 +1000

    Use master for Machines

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "ssh://git.mipal.net/Users/Shared/git/Attributes.git", .branch("master")),
-        .package(url: "ssh://git.mipal.net/Users/Shared/git/Machines.git", .branch("filewrappers"))
+        .package(url: "ssh://git.mipal.net/Users/Shared/git/Machines.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
