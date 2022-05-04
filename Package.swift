259e6ae351b3292ecc673bd08926c6b655dc5aba
Callum McColl <callum.mccoll@griffithuni.edu.au>
2022-05-04 13:57:40 +1000

    Use github urls in Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "ssh://git.mipal.net/Users/Shared/git/Attributes.git", .branch("master")),
+        .package(url: "git@github.com:mipalgu/Attributes.git", .branch("main")),
         .package(url: "https://github.com/mipalgu/Machines.git", .branch("main"))
     ],
     targets: [
