af7dafc6fe7863ecf7a8dc727a234c3787c2354b
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-27 18:05:51 +1000

    Linted includes.

--- Sources/MetaMachines/VHDLMachines/schema/VHDLIncludes.swift
+++ Sources/MetaMachines/VHDLMachines/schema/VHDLIncludes.swift
@@ -5,22 +5,29 @@
 //  Created by Morgan McColl on 23/7/21.
 //
 
-import Foundation
 import Attributes
+import Foundation
 
+/// The includes group that allows user to specify the includes for the VHDL code. This group also allows
+/// users to add in custom code in the architecture head and body.
 struct VHDLIncludes: GroupProtocol {
-    
-    public typealias Root = MetaMachine
-    
+
+    /// The root is a meta machine.
+    typealias Root = MetaMachine
+
+    /// This group is located at index 2 in the attributes array of the meta machine.
     let path = MetaMachine.path.attributes[2]
-    
+
+    /// The includes of this machine.
     @CodeProperty(label: "includes", language: .vhdl)
     var includes
-    
+
+    /// The custom architecture head code of this machine.
     @CodeProperty(label: "architecture_head", language: .vhdl)
     var architectureHead
-    
+
+    /// The custom architecture body code of this machine.
     @CodeProperty(label: "architecture_body", language: .vhdl)
     var architectureBody
-    
+
 }
