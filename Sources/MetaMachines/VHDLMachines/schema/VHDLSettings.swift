104e52e19d705dda2062b68963b8602369111826
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-28 16:41:32 +1000

    Linted settings.

--- Sources/MetaMachines/VHDLMachines/schema/VHDLSettings.swift
+++ Sources/MetaMachines/VHDLMachines/schema/VHDLSettings.swift
@@ -5,26 +5,32 @@
 //  Created by Morgan McColl on 23/7/21.
 //
 
-import Foundation
 import Attributes
+import Foundation
 
+/// A group for additional machine settings. This group specifies the initial and suspended state of the
+/// machine.
 struct VHDLSettings: GroupProtocol {
-    
-    public typealias Root = MetaMachine
-    
+
+    /// The root is a ``MetaMachine``.
+    typealias Root = MetaMachine
+
+    /// The path to the group containing these settings.
     let path = MetaMachine.path.attributes[3]
-    
+
+    /// A property for setting the suspended state of the machine.
     @EnumeratedProperty(
         label: "suspended_state",
         validValues: []
     )
     var suspendedState
-    
+
+    /// A property for setting the initial state of the machine.
     @EnumeratedProperty(
         label: "initial_state",
         validValues: [],
         validation: { $0.notEmpty() }
     )
     var initialState
-    
+
 }
