cdc63110a73d520d407a5c9b03fb09dca590f899
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-30 00:16:56 +1000

    Linted file.

--- Sources/MetaMachines/VHDLMachines/schema/VHDLStateSchema.swift
+++ Sources/MetaMachines/VHDLMachines/schema/VHDLStateSchema.swift
@@ -5,19 +5,23 @@
 //  Created by Morgan McColl on 28/7/21.
 //
 
-import Foundation
 import Attributes
+import Foundation
 
+/// The schema for a VHDL state.
 struct VHDLStateSchema: SchemaProtocol {
-    
+
+    /// The root is a ``MetaMachine``.
     typealias Root = MetaMachine
-    
-    //Need to create triggers for state CUD operations
-    
+
+    // Need to create triggers for state CUD operations
+
+    /// The variables local to the state.
     @Group
     var variables = VHDLStateVariables()
-    
+
+    /// The actions executed in the state.
     @Group
     var actions = VHDLStateActions()
-    
+
 }
