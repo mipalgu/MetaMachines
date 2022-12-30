0936fd60f4f4db0f08efeb68266fd6128b715652
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-30 18:24:35 +1000

    Linted MachineSchema.

--- Sources/MetaMachines/MachineSchema.swift
+++ Sources/MetaMachines/MachineSchema.swift
@@ -8,16 +8,30 @@
 import Attributes
 import Foundation
 
+/// A schema that represents a ``MetaMachine``. This schema is similar to the standard `SchemaProtocol` found
+/// in `Attributes`, however it has additional properties for the state and transition schemas.
 public protocol MachineSchema: SchemaProtocol, MachineMutatorResponder where Root == MetaMachine {
-    
+
+    /// The type of the state schema.
     associatedtype StateSchema: SchemaProtocol where StateSchema.Root == Root
-    
+
+    /// The type of the transition schema.
     associatedtype TransitionSchema: SchemaProtocol where TransitionSchema.Root == Root
-    
+
+    /// The state schema.
     var stateSchema: StateSchema { get }
-    
+
+    /// The transition schema.
     var transitionSchema: TransitionSchema { get }
 
+    /// Initialise this schema from a ``MetaMachine``s properties.
+    /// - Parameters:
+    ///   - name: The name of the machine.
+    ///   - initialState: The name of the initial state in the machine.
+    ///   - states: The states of the machine.
+    ///   - dependencies: The dependencies of the machine.
+    ///   - attributes: The attributes in the machine.
+    ///   - metaData: The metadata in the machine.
     init(
         name: String,
         initialState: StateName,
@@ -29,53 +43,85 @@ public protocol MachineSchema: SchemaProtocol, MachineMutatorResponder where Roo
 
 }
 
+/// Default implementations.
 public extension MachineSchema {
 
     /// The trigger is all the group triggers by default.
     var trigger: AnyTrigger<Root> {
         AnyTrigger(groups.map(\.allTriggers) + [stateSchema.trigger, transitionSchema.trigger])
     }
-    
-    mutating func didCreateDependency(machine: inout MetaMachine, dependency: MachineDependency, index: Int) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didCreateDependency(
+        machine: inout MetaMachine, dependency: MachineDependency, index: Int
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didCreateNewState(machine: inout MetaMachine, state: State, index: Int) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didCreateNewState(
+        machine: inout MetaMachine, state: State, index: Int
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didChangeStatesName(machine: inout MetaMachine, state: State, index: Int, oldName: String) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didChangeStatesName(
+        machine: inout MetaMachine, state: State, index: Int, oldName: String
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didCreateNewTransition(machine: inout MetaMachine, transition: Transition, stateIndex: Int, transitionIndex: Int) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didCreateNewTransition(
+        machine: inout MetaMachine, transition: Transition, stateIndex: Int, transitionIndex: Int
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didDeleteDependency(machine: inout MetaMachine, dependency: MachineDependency, at: Int) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didDeleteDependency(
+        machine: inout MetaMachine, dependency: MachineDependency, at: Int
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didDeleteState(machine: inout MetaMachine, state: State, at: Int) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didDeleteState(
+        machine: inout MetaMachine, state: State, at: Int
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didDeleteTransition(machine: inout MetaMachine, transition: Transition, stateIndex: Int, at: Int) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didDeleteTransition(
+        machine: inout MetaMachine, transition: Transition, stateIndex: Int, at: Int
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didDeleteDependencies(machine: inout MetaMachine, dependency: [MachineDependency], at: IndexSet) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didDeleteDependencies(
+        machine: inout MetaMachine, dependency: [MachineDependency], at: IndexSet
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didDeleteStates(machine: inout MetaMachine, state: [State], at: IndexSet) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didDeleteStates(
+        machine: inout MetaMachine, state: [State], at: IndexSet
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
-    mutating func didDeleteTransitions(machine: inout MetaMachine, transition: [Transition], stateIndex: Int, at: IndexSet) -> Result<Bool, AttributeError<MetaMachine>> {
-        return .success(false)
+
+    /// Default implementation does nothing.
+    mutating func didDeleteTransitions(
+        machine: inout MetaMachine, transition: [Transition], stateIndex: Int, at: IndexSet
+    ) -> Result<Bool, AttributeError<MetaMachine>> {
+        .success(false)
     }
-    
+
+    /// Default implementation does nothing.
     mutating func update(from metaMachine: MetaMachine) {}
-    
+
 }
