b29bd44e10d8d6c214448d61066ba213b61afb13
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2023-02-15 02:20:02 +1000

    Updated dependency versions.

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.1.0"),
-        .package(url: "git@github.com:mipalgu/Machines.git", from: "2.0.0"),
-        .package(url: "git@github.com:mipalgu/Attributes.git", from: "1.0.0")
+        .package(url: "git@github.com:mipalgu/Machines.git", from: "3.0.0"),
+        .package(url: "git@github.com:mipalgu/Attributes.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
